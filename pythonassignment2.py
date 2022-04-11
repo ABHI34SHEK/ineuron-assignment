983b77d1bb7f5abc9c01b569c2a5909b06cebd2f
abhishek mishra <86375333+ABHI34SHEK@users.noreply.github.com>
2022-04-12 02:13:23 +0530

    Create pythonassignment2.py

---
+++ pythonassignment2.py
@@ -0,0 +1,15 @@
+#question 1
+a = ("ineuron")
+print(a[::-1])
+ 
+  
+#question 2
+for a in range(0,4):
+    for b in range(0,a+1):
+        print("* ",end="")
+    print("\r")
+    
+for i in range(0,5):
+    for j in range(i,5):
+        print("* ",end="")
+    print("\r")
